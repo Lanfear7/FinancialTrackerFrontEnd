9449896bd053f0f63b5c1fc15210baa1dde286d5
Brennan <brennan1086@gmail.com>
2023-01-15 21:07:55 -0800

    Mobile landing page styling done + animations

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,25 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 import Logo from '../Images/DriftLogo.png'
 import { VscMenu } from 'react-icons/vsc'
 import { AiOutlineClose } from 'react-icons/ai'
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 
 function NavBar() {
 
     const [sideNav, setSideNav] = useState(false)
 
+    const { height, width } = useWindowDimensions();
+    
   return (
     <nav className='bg-FTwhite w-full h-24 flex items-center'>
         <figure className='m-0 w-24 h-full'>
             <img className='w-full h-full object-cover' src={Logo}></img>
         </figure>
         <h1 className='text-FTgray text-2xl'>Drift Finances</h1>
-        <div className='absolute top-5 right-5 h-7 w-7' onClick={()=>setSideNav(true)}>
-            <VscMenu className='h-full w-full' />
-        </div>
-
+        {
+            width < 900 &&
+            <div className='absolute top-5 right-5 h-7 w-7' onClick={()=>setSideNav(true)}>
+                <VscMenu className='h-full w-full' />
+            </div>
+        }
         {
             sideNav &&
-            <div className='absolute top-0 right-0 h-screen w-full bg-FTblack flex flex-wrap justify-end md:w-1/3 lg:w-1/4'>
+            <div className='absolute top-0 right-0 h-screen w-full bg-FTblack flex flex-wrap justify-end md:w-2/4 z-30'>
                 <AiOutlineClose className=' mr-3 mt-3 w-6 h-6 bg-red-500 rounded z-20' onClick={()=>{setSideNav(false)}}/>
                 <ul className='absolute h-full w-full flex justify-evenly flex-wrap'>
                     <li className='basis-full flex items-center justify-center text-FTgreen text-2xl border-b border-FTgray hover:bg-FTgray'>Dashboard</li>
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
+import { useSpring, animated } from '@react-spring/web'
 
 function LandingPage() {
+    const textAnimation = useSpring({
+        from: {y: 200},
+        to: {y: -50},
+        config:{
+            duration: 800
+        }
+    })
+    const buttonFadeInAnimation = useSpring({
+        from: {opacity: 0},
+         to: { opacity : 1},
+         delay: 900
+    })
   return (
-    <div className='w-full min-h-screen bg-FTblack'>
-        <NavBar />
+    <div className='w-full min-h-screen bg-FTblack flex flex-wrap'>
+        <NavBar className='basis-full'/>
+        <div className='h-48 w-full flex flex-wrap justify-center'>
+            <animated.div
+            style={textAnimation}
+            >
+                <h1 className='text-center basis-full text-FTgreen text-4xl'>Welcome to Drift</h1>
+                <h1 className='text-center basis-full text-FTgreen text-xl'>One stop for tracking all your finances</h1>
+            </animated.div>
+            <div className='bg-FTblack h-full w-full relative z-10 flex justify-center items-center'>
+                <animated.div 
+                style={buttonFadeInAnimation}
+                className='w-full flex justify-around'>
+                    <button className='bg-FTgray px-5 py-1 rounded-lg text-FTwhite'>SignUp</button>
+                    <button className='bg-FTgray px-5 py-1 rounded-lg text-FTwhite'>LogIn</button>
+                </animated.div>
+            </div>
+            
+        </div>
     </div>
   )
 }
