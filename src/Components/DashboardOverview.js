796f6560fe19c551e8ee13b63b7c3df3da7e54c0
Brennan <brennan1086@gmail.com>
2023-01-21 01:52:59 -0800

    Users can update monthly income

--- src/Components/DashboardOverview.js
+++ src/Components/DashboardOverview.js
@@ -1,10 +1,11 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { Chart } from "react-google-charts";
 import { useSelector } from 'react-redux'
 
 function DashboardOverview() {
 
-    const [monthlyIncome, setMonthlyIncome] = useState(false);
+    const [monthlyIncome, setMonthlyIncome] = useState(0);
     const [monthlySavings, setMonthlySavings] = useState(0.00);
   
     const {JWT} = useSelector((state) => state.User)
@@ -22,34 +23,39 @@ function DashboardOverview() {
     };
   
     const data = [
-      ["Task", "Hours per Day"],
-      ["Work", 11],
-      ["Eat", 2],
-      ["Commute", 2],
-      ["Watch TV", 2],
-      ["Sleep", 7],
+      ["Expense", "Expenses Per Month"],
+      ["Transportation", 1200],
+      ["Food", 500],
+      ["Entertainment", 150],
+      ["Bills", 900],
+      ["other", 300],
+      ["Savings", 250]
     ];
 
   return (
     <div className='h-full border-b border-FTgreen pb-32 md:pb-16'>
-          <div className='flex flex-wrap mt-6 justify-between lg:items-center h-[400px] lg:pl-16'>
-            <div className='h-[200px] pl-6 w-[290px] flex flex-wrap md:h-full items-center'>
-              <div className='text-FTwhite border-2 border-FTgreen bg-FTgray rounded-lg h-[75px] p-2 shadow-lg w-full md:h-[150px] flex flex-wrap items-center'>
+          <div className='flex flex-wrap mt-6 justify-between md:justify-around lg:items-center h-[500px] lg:pl-16'>
+            <div className='h-[300px] pl-6 w-[290px] md:w-[350px] flex flex-wrap md:h-full items-center'>
+              <div className='text-FTwhite border-2 border-FTgreen bg-FTgray rounded-lg h-[85px] p-2 shadow-lg w-full md:h-[150px] flex flex-wrap items-center'>
                 <h1 className='basis-full text-lg md:text-2xl'>Monthly Savings</h1>
-                <h1>${monthlySavings.toFixed(2)}</h1>
+                <h1 className='md:pl-5 text-lg'>${monthlySavings.toFixed(2)}</h1>
               </div>
-              <div className='text-FTwhite border-2 border-FTgreen bg-FTgray rounded-lg h-[75px] p-2 shadow-lg w-full md:h-[150px] flex flex-wrap items-center'>
+              <div className='text-FTwhite border-2 border-FTgreen bg-FTgray rounded-lg h-[85px] p-2 shadow-lg w-full md:h-[150px] flex flex-wrap items-center'>
                 <h1 className='basis-full text-lg md:text-2xl'>Monthly Income</h1>
                 {
-                  monthlyIncome ? 
-                    <h1>{}</h1>
-                  :
-                  <div className='flex justify-between w-full'>
-                    <input type='text'></input>
-                    <button className='bg-FTgreen text-FTblack px-3 rounded-md'>Enter</button>
+                  <div className='flex justify-evenly w-full'>
+                    <input type='text' className='bg-FTgray border-b border-FTgreen' placeholder='Enter Monthly Income' onChange={(e)=> setMonthlyIncome(e.target.value)}></input>
+                    <button className='bg-FTgreen text-FTblack px-3 rounded-md' onClick={()=> UpdateIncome()}>Enter</button>
                   </div>
-                    
                 }
+              </div>
+              <div className='text-FTwhite border-2 border-FTgreen bg-FTgray rounded-lg h-[85px] p-2 shadow-lg w-full md:h-[150px] flex flex-wrap items-center'>
+                <h1 className='basis-full text-lg md:text-2xl'>Expense</h1>
+                <div className='flex flex-wrap justify-evenly w-full'>
+                  <input type='text' className='bg-FTgray border-b border-FTgreen basis-[80%]' placeholder='Enter Expense'></input>
+                  <input type='text' className='bg-FTgray border-b border-FTgreen basis-[80%]' placeholder='Enter Cost'></input>
+                  <button className='bg-FTgreen text-FTblack px-3 rounded-md basis-[25%] mt-3'>Enter</button>
+                </div>
               </div>
             </div>
             <div className='basis-full md:basis-1/2 h-[300px] w-[300px] md:h-[400px] sm:w-[300px] flex justify-center'>
@@ -64,6 +70,36 @@ function DashboardOverview() {
           </div>
         </div>
   )
+
+  function UpdateIncome()
+  {
+    const config = {
+      headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${JWT}`, 
+      }
+    };
+
+    const bodyParameters = {
+       Income: monthlyIncome
+    };
+
+    axios.post(`https://localhost:44320/api/Dashboard/CurrentUser/UpdateIncome/${user['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']}`,
+    bodyParameters,
+    config
+    )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  }
+
 }
+
+
 
 export default DashboardOverview
